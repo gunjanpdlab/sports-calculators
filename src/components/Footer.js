6cdef216bf3c352801df2720b829051f52a6fd56
gunjanpdlab <102739937+gunjanpdlab@users.noreply.github.com>
2023-05-22 10:38:17 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -27,17 +27,15 @@ export default function StickyFooter() {
       <Grid item lg={12} md={12} sm={12} xs={12}>
         <Box className="footer-main">
       <Box fontSize= "15px" paddingTop="10px" paddingBottom="5px" marginLeft="5px">Sports Betting Calculators</Box>
-        <Box fontSize="15px" mb={2}>© Copyright - topbetcalculator.com (2020) - Top Bet Calculator <span className="terms-conditions"> <Link href="#" onClick={onClickTerms} className="terms-setting">
+        <Box fontSize="15px" mb={2}>© Copyright - BettyBoo (2023) - Top Bet Calculator <span className="terms-conditions"> <Link href="#" onClick={onClickTerms} className="terms-setting">
             Terms {"&"} Conditions
         </Link>
-        <Link href="mailto:joshua@psychotechnology.com">
-            Contact Us
-        </Link></span></Box>
+       </span></Box>
         </Box>
        
         <div style={{ display: openTerms ? 'inline-block' : 'none' }}>
             <Divider />
-            <Typography variant="body2">TopBetCalculator disclaims all liabilities regarding the content of these calculators and any use thereof that could be made by any person. TopBetCalculator do not make any representations or warranties, express or implied, as to the accuracy, completeness, or fitness for any purpose or use of the content provided. Accordingly, you should not rely on any of the information as authoritative or as a substitute for the exercise of your own skill and judgment in making a trade or other decision.</Typography>
+            <Typography variant="body2">BettyBoo disclaims all liabilities regarding the content of these calculators and any use thereof that could be made by any person. BettyBoo do not make any representations or warranties, express or implied, as to the accuracy, completeness, or fitness for any purpose or use of the content provided. Accordingly, you should not rely on any of the information as authoritative or as a substitute for the exercise of your own skill and judgment in making a trade or other decision.</Typography>
         </div>
 			</Grid>
       
